d135557a237b771d4875f7347dcce11857d10c01
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-06 16:27:46 -0600

    Use Kent's suggestions: useState for entire return value of hook

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -2,52 +2,42 @@
 // http://localhost:3000/location
 
 import * as React from 'react'
-import {render, screen, act, waitForElementToBeRemoved} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
+import {} from '@testing-library/react-hooks'
 import Location from '../../examples/location'
+import {useCurrentPosition} from 'react-use-geolocation'
 
-beforeAll(() => {
-  window.navigator.geolocation = {
-    getCurrentPosition: jest.fn(),
-  }
-})
-
-function deferred() {
-  let resolve, reject
-  const promise = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-  return {promise, resolve, reject}
-}
+jest.mock('react-use-geolocation')
 
 test('displays the current location of the user', async () => {
-  const fakePosition = {
+  let setReturnValue
+  function useMockCurrentPosition() {
+    const state = React.useState([])
+    setReturnValue = state[1]
+    return state[0]
+  }
+  useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
+  render(<Location />)
+  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+  const mockPosition = {
     coords: {
       latitude: 32,
       longitude: 129,
     },
   }
 
-  const {promise, resolve} = deferred()
-
-  window.navigator.geolocation.getCurrentPosition.mockImplementation((success, error) => {
-    promise.then(() => success(fakePosition))
-  })
-
-  render( <Location /> )
-  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
-
-  await act(async () => {
-    resolve()
-    await promise
+  act(() => {
+    setReturnValue([mockPosition])
   })
 
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
   expect(screen.getByText(/latitude/i)).toHaveTextContent(
-    `Latitude: ${fakePosition.coords.latitude}`
+    `Latitude: ${mockPosition.coords.latitude}`
   )
   expect(screen.getByText(/longitude/i)).toHaveTextContent(
-    `Longitude: ${fakePosition.coords.longitude}`
+    `Longitude: ${mockPosition.coords.longitude}`
   )
 })
 
