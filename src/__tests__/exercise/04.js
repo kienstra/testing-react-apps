6258a05cc0bfd7d25e6f4b4c81411626edd1a70b
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-06 11:56:25 -0600

    EC2: Use faker npm package

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -2,16 +2,24 @@
 // http://localhost:3000/login
 
 import * as React from 'react'
+import faker from 'faker'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+
+
+function buildLoginForm() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   const mockHandleSubmit = jest.fn()
   render(<Login onSubmit={mockHandleSubmit} />)
 
-  const username = 'obasidsfs'
-  const password = 'iguwbiwef'
+  const {username, password} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
